d364a567c1d1e8778f95675178a8de58ef8e8ce7
Rizal Widyarta Gowandy <rizal.gow@gmail.com>
2020-06-20 16:05:16 +0700

    Add echosimple

---
+++ echosimple/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/rizalgowandy/go-swag-sample/docs/echosimple"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+// @title Echo Swagger Example API
+// @version 1.0
+// @description This is a sample server server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:3000
+// @BasePath /
+// @schemes http
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+
+	// Routes
+	e.GET("/", HealthCheck)
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+// HealthCheck godoc
+// @Summary Show the status of server.
+// @Description get the status of server.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} map[string]interface{}
+// @Router / [get]
+func HealthCheck(c echo.Context) error {
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"data": "Server is up and running",
+	})
+}
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/rizalgowandy/go-swag-sample
+
+go 1.14
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/swaggo/echo-swagger v1.0.0
+	github.com/swaggo/swag v1.6.3
+)
